578b3c6f132fbc0c4103d4388ba645da5f6c1491
cumedang <yusun2234sun@gmail.com>
2024-04-03 19:45:33 +0900

    자바

---
+++ src/main/frontend/js/moludi.js
@@ -0,0 +1,87 @@
+async function fetchFoods() {
+  try {
+      const response = await fetch('http://localhost:8081/foods');
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error('Error fetching foods:', error);
+  }
+}
+
+async function renderFoodItems() {
+  const foods = await fetchFoods();
+  const foodDiv = document.getElementById('foods');
+  const searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+  foodDiv.innerHTML = ''; // 검색 결과를 업데이트하기 전에 이전 결과를 지웁니다.
+
+  foods.forEach(food => {
+      // 검색어가 포함된 결과만 보여줍니다.
+      if (food.name.toLowerCase().includes(searchInput)) {
+          const foodItemDiv = document.createElement('div');
+          foodItemDiv.classList.add('food-item');
+
+          const radioInput = document.createElement('input');
+          radioInput.type = 'radio';
+          radioInput.name = 'food'; // 라디오 그룹의 이름 설정
+          radioInput.value = food.name; // 라디오 버튼의 값으로 음식의 이름 설정
+          radioInput.classList.add('food-radio');
+          radioInput.id = `radio-${food.name}`; // 라디오 버튼 id 설정
+
+          const imgBoxDiv = document.createElement('div');
+          imgBoxDiv.classList.add('img-box');
+
+          const imgElement = document.createElement('img');
+          imgElement.src = food.link;
+          imgElement.alt = food.name;
+
+          const foodName = document.createElement('p');
+          foodName.classList.add('food-name');
+          foodName.textContent = food.name;
+
+          const foodDetails = document.createElement('div');
+          foodDetails.classList.add('food-detail');
+
+          const detailLabels = ['칼로리', '탄수화물', '단백빌', '지방', '비타민'];
+          const detailValues = [food.calories, food.carbohydrate, food.protein, food.province, food.vitamin];
+
+          detailLabels.forEach((label, index) => {
+              const detailLabel = document.createElement('span');
+              detailLabel.classList.add('detail-label');
+              detailLabel.textContent = label + ': ';
+
+              const detailValue = document.createElement('span');
+              detailValue.classList.add('detail-value');
+              detailValue.textContent = detailValues[index];
+
+              const detailContainer = document.createElement('div');
+              detailContainer.appendChild(detailLabel);
+              detailContainer.appendChild(detailValue);
+
+              foodDetails.appendChild(detailContainer);
+          });
+
+          imgBoxDiv.appendChild(imgElement);
+          imgBoxDiv.appendChild(foodName);
+          imgBoxDiv.appendChild(foodDetails);
+
+
+          let foodRadio = document.querySelectorAll(".food-radio");
+
+          const labelElement = document.createElement('label');
+          labelElement.setAttribute('for', `radio-${food.name}`); // 라벨을 라디오 버튼과 연결
+          foodItemDiv.appendChild(radioInput);
+          foodItemDiv.appendChild(labelElement);
+          labelElement.appendChild(imgBoxDiv);
+
+          foodDiv.appendChild(foodItemDiv);
+      }
+  });
+
+}
+
+function searchFunction() {
+  renderFoodItems(); // 입력이 발생할 때마다 결과를 다시 렌더링합니다.
+}
+
+renderFoodItems(); // 페이지가 로드될 때 한 번 렌더링합니다.
---
+++ src/test/java/gbsw/hs/kr/Health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package gbsw.hs.kr.Health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
